d975d0a759f12f9880d32cd13866b401ab23d31a
Asuka <asuka@Asukas-MacBook-Air.local>
2021-12-02 20:36:32 -0500

    add part 2

--- day-1/index.js
+++ day-1/index.js
@@ -1,13 +1,30 @@
 const input = require("./input");
-
-function countIncrease(depthList) {
+// part 1
+const countIncrease = (depthList) => {
   let count = 0;
   depthList.reduce((prev, current) => {
     prev < current && count++;
     return current;
   });
   return count;
-}
+};
 
-const result = countIncrease(input);
-console.log(result);
+const resultPart1 = countIncrease(input);
+console.log(resultPart1);
+
+// part 2
+const countWindowIncrease = (depthList) => {
+  let count = 0;
+  depthList.forEach((depth, index) => {
+    const firstWindow =
+      depthList[index] + depthList[index + 1] + depthList[index + 2];
+    const secondWindow =
+      depthList[index + 1] + depthList[index + 2] + depthList[index + 3];
+
+    firstWindow < secondWindow && count++;
+  });
+  return count;
+};
+
+const resultPart2 = countWindowIncrease(input);
+console.log(resultPart2);
