fa8368a3e73d9a19645d3f2649739046a1aa6a2a
Asuka <asuka@Asukas-MacBook-Air.local>
2021-12-02 21:23:35 -0500

    part 2 day 2 complete

--- day-2/index.js
+++ day-2/index.js
@@ -23,5 +23,32 @@ const moveSubmarine = (position, instructions) => {
   return position.vertical * position.horizontal;
 };
 
-const result = moveSubmarine(position, input);
-console.log(result);
+const resultPart1 = moveSubmarine(position, input);
+console.log(resultPart1);
+
+// Part 2
+// position = { horizontal: 0, vertical: 0, aim: 0 }
+// up/down only changes the aim
+// forward changes the position according to the aim
+// horizontal => forward value
+// vertical => forward value * aim
+// ... same for the rest
+
+const position2 = { horizontal: 0, vertical: 0, aim: 0 };
+const moveSubmarineWithAim = (position, instructions) => {
+  instructions.map((instruction) => {
+    const [command, value] = instruction.split(" ");
+    if (command === "forward") {
+      position.horizontal += parseInt(value);
+      position.vertical += parseInt(value) * position.aim;
+    } else if (command === "up") {
+      position.aim -= parseInt(value);
+    } else if (command === "down") {
+      position.aim += parseInt(value);
+    }
+  });
+  return position.vertical * position.horizontal;
+};
+
+const resultPart2 = moveSubmarineWithAim(position2, input);
+console.log(resultPart2);
